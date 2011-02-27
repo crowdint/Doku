ce87f75494a131f279d0de94d79bee69696b866f
David Padilla <david@crowdint.com>
2011-02-27 00:03:13 -0800

    Basic login form

--- lib/application.rb
+++ lib/application.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'hotcocoa'
+require 'gdocs4ruby'
 
 # Replace the following code with your own hotcocoa code
 
@@ -10,7 +11,7 @@ class Application
   def start
     application :name => "Docbrown" do |app|
       app.delegate = self
-      window :size => [640, 480], :center => true, :view => :nolayout do |win|
+      window :size => [340, 180], :center => true, :view => :nolayout do |win|
         win.will_close { exit }
 
         win.view = layout_view(:layout => {:expand => [:width, :height],
@@ -19,13 +20,29 @@ class Application
           vert << layout_view(:frame => [0, 0, 0, 40], :mode => :horizontal,
                              :layout => {:padding => 0, :margin => 0,
                              :start => false, :expand => [:width]}) do |horiz|
-              horiz << label(:text => "Feed", :layout => {:align => :center})
 
-              horiz << @@feed_field = text_field(:layout => {:expand => :width})
+            horiz << label(:text => "User:", :layout => {:align => :center})
 
-              horiz << button(:title => "Go", :layout => {:align => :center}) do |b|
+            horiz << @@feed_field = text_field(:layout => {:expand => :width})
+          end
+
+          vert << layout_view(:frame => [0, 0, 0, 40], :mode => :horizontal,
+                             :layout => {:padding => 0, :margin => 0,
+                             :start => false, :expand => [:width]}) do |horiz|
+
+            horiz << label(:text => "Password:", :layout => {:align => :center})
+            horiz << @password = text_field(:layout => {:expand => :width})
+
+
+          end
+
+          vert << layout_view(:frame => [0, 0, 0, 40], :mode => :horizontal,
+                             :layout => {:padding => 0, :margin => 0,
+                             :start => false, :expand => [:width]}) do |horiz|
+
+            horiz << button(:title => "Go", :layout => {:align => :center}) do |b|
                 # Code to button click
-              end
+            end
           end
         end
       end
